554f02d1dde35ee8a51a676a6b71c99c3789adb2
Hussein Fawaz <hsnfwz99@gmail.com>
2022-11-16 21:09:18 -0800

    feature-1: added products

---
+++ src/components/ProductCardGrid.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  // types
+  import type ProductType from '@/src/types/ProductType';
+
+  // components
+  import ProductCard from '@/src/components/ProductCard.svelte';
+
+  // props
+  export let products: ProductType[] = [];
+</script>
+
+<div class="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
+  {#each products as product}
+    <ProductCard {product} />
+  {/each}
+</div>
