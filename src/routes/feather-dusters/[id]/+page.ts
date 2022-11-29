95cc17cace67a0ae0e4ab1deebf2f708fa678eec
Hussein Fawaz <hsnfwz99@gmail.com>
2022-11-28 18:48:02 -0800

    main: removed log

--- src/routes/feather-dusters/[id]/+page.ts
+++ src/routes/feather-dusters/[id]/+page.ts
@@ -7,8 +7,6 @@ import type I_ProductTableRecord from '$interfaces/I_ProductTableRecord';
 export async function load({ params }) {
   const product: I_ProductTableRecord | undefined = await getProductById(params.id);
 
-  console.log('PRODUCT', product)
-
   return {
     product,
   };
