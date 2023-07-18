4be58a49a1e75b531cfc1fac2c202d78ee249ad8
Hussein Fawaz <hsnfwz99@gmail.com>
2023-07-17 21:32:55 -0700

    clean hero

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -12,13 +12,16 @@
     class="h-[calc(100vh-40px-72.25px)] w-full object-cover"
   />
   <div
-    class="absolute left-0 top-1/3 flex w-full flex-col items-center justify-center gap-4 self-center px-8 py-2 text-center text-white"
+    class="absolute left-1/2 top-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-8 self-start px-8 py-2 text-center text-white"
   >
-    <p class="montserrat-bold text-4xl">Ostrich Feathers</p>
-    <p>Handmade from 100% natural farm-raised ostrich</p>
+    <div class="flex flex-col gap-4">
+      <p class="montserrat-bold text-4xl">Ostrich Feathers</p>
+      <p>From 100% natural farm-raised ostrich</p>
+    </div>
+
     <Link
       href="/products/feathers"
-      customClass="px-4 py-2 bg-white rounded text-black">Shop Feathers</Link
+      customClass="px-4 py-2 bg-white rounded text-black">Shop Now</Link
     >
   </div>
 </div>
